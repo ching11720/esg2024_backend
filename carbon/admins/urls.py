cf31f0249d1ce39be12e524d905028355adeabce
ching11720 <jasmine910402614@gmail.com>
2024-08-01 00:29:14 +0800

    test(DB):add some testing data and update the table settings

--- carbon/admins/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create_user', views.create_user, name="create_user")
-]
--- carbon/login/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login_user, name="login"),
-    path('revise_password', views.revise_password, name="revise_password"),
-]
